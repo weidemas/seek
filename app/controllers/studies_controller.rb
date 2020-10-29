9f409683ac614abef129cab9c4e6432c05a6a3a8
jdestin <jeremy.destin@inra.fr>
2020-10-29 16:21:25 +0100

    Merge the code from previous version.

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -121,16 +121,23 @@ class StudiesController < ApplicationController
   def batch_uploader; end
 
   def preview_content
+    user_uuid = if User.current_user
+                  User.current_user.attributes['uuid'].to_s
+                else
+                  'user_uuid'
+                end
     tempzip_path = params[:content_blobs][0][:data].tempfile.path
-    data_files, studies = Study.unzip_batch(tempzip_path)
-    study_filename = studies.first.name
+    data_files, studies = Study.unzip_batch(tempzip_path, user_uuid)
+    study_filename = File.basename(studies.first.to_s)
     studies_file = ContentBlob.new
-    studies_file.tmp_io_object=File.open("#{Rails.root}/tmp/#{study_filename}")
-    studies_file.original_filename="#{study_filename}"
+    studies_file.tmp_io_object = File.open("#{Rails.root}/tmp/#{user_uuid}_studies_upload/#{study_filename}")
+    studies_file.original_filename = "#{study_filename}"
     studies_file.save!
     @studies = Study.extract_studies_from_file(studies_file)
     @study = @studies[0]
     @studies_datafiles = Study.extract_study_data_from_file(studies_file)
+    @license = Study.get_license(studies_file)
+    @existing_studies = JSON.parse(Study.get_existing_studies(@studies))
     render 'studies/batch_preview'
   end
 
@@ -139,20 +146,88 @@ class StudiesController < ApplicationController
     # e.g: Study.new(title: 'title', investigation: investigations(:metabolomics_investigation), policy: Factory(:private_policy))
     # study.policy = Policy.create(name: 'default policy', access_type: 1)
     # render plain: params[:studies].inspect
+    user_uuid = if User.current_user
+                  User.current_user.attributes['uuid'].to_s
+                else
+                  'user_uuid'
+                end
+    metadata_types = CustomMetadataType.where(title: 'MIAPPE metadata', supported_type: 'Study')
     studies_length = params[:studies][:title].length
-    batch_uploaded = false
-    studies_length.times do |index|
-      study_params = {
-        title: params[:studies][:title][index],
-        description: params[:studies][:description][index],
-        investigation_id: params[:study][:investigation_id],
-        person_responsible_id: params[:study][:person_responsible_id]
-      }
-      @study = Study.new(study_params)
-      batch_uploaded = true if @study.save
+    studies_uploaded = false
+    data_file_uploaded = false
+
+    unless params[:existing_studies].blank?
+      remove_existing_studies(params[:existing_studies])
     end
 
+
+    studies_length.times do |index|
+      metadata = generate_metadata(params[:studies], index)
+      study_params = {
+          title: params[:studies][:title][index],
+          description: params[:studies][:description][index],
+          investigation_id: params[:study][:investigation_id],
+          person_responsible_id: params[:study][:person_responsible_id],
+          custom_metadata: CustomMetadata.new(
+              custom_metadata_type: metadata_types.last,
+              data: metadata
+          )
+      }
+      @study = Study.new(study_params)
+      Study.check_study_is_valid(@study, metadata)
+      if @study.valid? && @study.save! && @study.custom_metadata.valid?
+        studies_uploaded = true if @study.save
+
+        data_file_names = params[:studies][:data_files][index].split(', ')
+        data_file_names.length.times do |data_file_index|
+          study_metadata_id = params[:studies][:id][index]
+          study_id = CustomMetadata.where('json_metadata LIKE ?', "%\"id\":\"#{study_metadata_id}\"%").last.item_id
+          assay_class_id = AssayClass.where(title: 'Experimental assay').first.id
+          data_file_description = params[:studies][:data_file_description][index].split(', ')
+          assay_params = {
+              title: 'Assay for ' + params[:studies][:id][index] + '-' + (data_file_index + 1).to_s,
+              description: data_file_description[data_file_index],
+              study_id: study_id,
+              assay_class_id: assay_class_id
+          }
+
+          data_file_name = "#{data_file_names[data_file_index]}.csv"
+          data_file_url = "#{Rails.root}/tmp/#{user_uuid}_studies_upload/data/#{data_file_name}"
+          data_file_content_blob = ContentBlob.new
+          data_file_content_blob.tmp_io_object = File.open(data_file_url)
+          data_file_content_blob.original_filename = "#{data_file_name}"
+
+          license = params[:studies][:license]
+          data_file_params = {
+              title: data_file_names[data_file_index],
+              description: data_file_description[data_file_index],
+              license: license,
+              projects: Project.where(title: 'Default Project'),
+              content_blob: data_file_content_blob
+          }
+
+          assay_asset_params = {
+              assay: Assay.new(assay_params),
+              asset: DataFile.new(data_file_params)
+          }
+          @assay_asset = AssayAsset.new(assay_asset_params)
+
+          if @assay_asset.valid? && @assay_asset.save!
+            data_file_uploaded = true if @assay_asset.save
+          end
+        end
+      end
+    end
+
+    batch_uploaded = studies_uploaded && data_file_uploaded
+
     if batch_uploaded
+      user_uuid = if User.current_user
+                    User.current_user.attributes['uuid'].to_s
+                  else
+                    'user_uuid'
+                  end
+      FileUtils.rm_r("#{Rails.root}/tmp/#{user_uuid}_studies_upload/")
       respond_to do |format|
         flash[:notice] = "The #{t('studies')} were successfully created.<br/>".html_safe
         format.html { redirect_to studies_path }
@@ -164,6 +239,48 @@ class StudiesController < ApplicationController
     end
   end
 
+  def generate_metadata(studies_meta_data, index)
+    metadata = {
+        id: studies_meta_data[:id][index],
+        study_start_date: studies_meta_data[:startDate][index],
+        study_end_date: studies_meta_data[:endDate][index] || '',
+        contact_institution: studies_meta_data[:contactInstitution][index],
+        geographic_location_country: studies_meta_data[:geographicLocationCountry][index],
+        experimental_site_name: studies_meta_data[:experimentalSiteName][index],
+        latitude: studies_meta_data[:latitude][index],
+        longitude: studies_meta_data[:longitude][index],
+        altitude: studies_meta_data[:altitude][index],
+        description_of_the_experimental_design: studies_meta_data[:descriptionOfTheExperimentalDesign][index],
+        type_of_experimental_design: studies_meta_data[:typeOfExperimentalDesign][index],
+        observation_unit_level_hierarchy: studies_meta_data[:observationUnitLevelHierarchy][index],
+        observation_unit_description: studies_meta_data[:observationUnitDescription][index],
+        description_of_growth_facility: studies_meta_data[:descriptionOfGrowthFacility][index],
+        type_of_growth_facility: studies_meta_data[:typeOfGrowthFacility][index],
+        cultural_practices: studies_meta_data[:culturalPractices][index]
+    }
+    metadata
+  end
+
+
+
+  def remove_existing_studies(studies)
+    existing_studies = JSON.parse(studies.to_json)
+
+    existing_studies.each do |study|
+      study_id = JSON.parse(study.gsub("=>",":"))["id"].to_i
+      metadata_id = JSON.parse(study.gsub("=>",":"))["metadata_id"].to_i
+
+      Study.where(id: study_id).delete_all
+      CustomMetadata.where(id: metadata_id).delete_all
+      assays = Assay.where(study_id: study_id)
+      assays.each do |assay|
+        AssayAsset.where(assay_id: assay.id).delete_all
+      end
+      assays.delete_all
+
+    end
+
+  end
 
   private
   def validate_person_responsible(p)
